d9ee6c60343f2adff535cbcf867707c2d0978b58
Paweł Żentała <zentala@gmail.com>
2025-04-27 22:20:22 +0200

    refactor: use vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,11 +5,11 @@ export default defineConfig({
   test: {
     globals: true, // Make describe, it, expect, etc. available globally
     environment: 'jsdom', // Use jsdom environment for React components
-    // setupFiles: './src/setupTests.ts', // Temporarily remove setup file from vitest config
+    setupFiles: './src/setupTests.ts', // Point to setup file
     // Optionally, you can configure coverage here
-    // coverage: {
-    //   provider: 'v8', // or 'istanbul'
-    //   reporter: ['text', 'json', 'html'],
-    // },
+    coverage: {
+      provider: 'v8', // or 'istanbul'
+      reporter: ['text', 'json', 'html'],
+    },
   },
 }); 
